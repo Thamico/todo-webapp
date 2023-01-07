aff2447f2d3acf43fdf09a3e1ccd2967eefdac73
Thamico <thanh.nguyenctn1999@gmail.com>
2023-01-07 19:39:45 +0100

    add and remove functionality + background images

--- src/addTask.js
+++ src/addTask.js
@@ -1,16 +1,42 @@
 function addTask(task, dueDate, priority, description) {
   const todoList = document.querySelector("#content");
-  console.log(todoList);
+
   // create a new list item
   const listItem = document.createElement("div");
+  listItem.classList.add("subdiv");
+  listItem.classList.add("card");
+
+  // create the "done" button
+  const doneButton = document.createElement("button");
+  doneButton.textContent = "Done";
+  doneButton.classList.add("done-button");
+
+  // attach a click event listener to the "done" button
+  doneButton.addEventListener("click", function () {
+    // remove the list item when the button is clicked
+    todoList.removeChild(listItem);
+  });
+
+  // add the task, description, priority, and due date to the list item
   listItem.innerHTML =
-    task + " " + dueDate + " " + priority + " " + description;
+    "Task: " +
+    task +
+    "<br>" +
+    "Description: " +
+    description +
+    "<br>" +
+    "Priority: " +
+    priority +
+    "<br>" +
+    "Due Date: " +
+    dueDate +
+    "<br>";
 
-  console.log(listItem);
+  // add the "done" button to the list item
+  listItem.appendChild(doneButton);
 
+  // add the list item to the todo list
   todoList.appendChild(listItem);
-
-  console.log(todoList);
 }
 
 export default addTask;
