0d4bf00570ae9bc8de9f1ae2546dc5b35006519c
Thamico <thanh.nguyenctn1999@gmail.com>
2023-01-07 13:41:22 +0100

    removed generate task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import generateTasks, { addTask } from "./generateTasks";
 
-addTask();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const task = e.elements.task.value;
+  const dueDate = e.elements.dueDate.value;
+  addTask(task, dueDate);
+});
